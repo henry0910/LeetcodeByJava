2232d95fef3dc55c390d6fa8d46c112b919a572d
tjshen94 <tianjie.shen@rhsmith.umd.edu>
2019-06-09 00:53:23 -0400

    solution203

--- src/Solution203.java
+++ src/Solution203.java
@@ -1,5 +1,9 @@
 public class Solution203 {
     public ListNode removeElements(ListNode head, int val) {
-
+        if (head == null) return null;
+        ListNode node = removeElements(head.next, val);
+        if (head.val == val) return node;
+        head.next = node;
+        return head;
     }
 }
